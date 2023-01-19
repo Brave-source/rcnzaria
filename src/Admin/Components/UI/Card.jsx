57b6066302bb77651beb49d4b30aa63f0db786a0
Cyril Arinze <ejicyrilarinze@gmail.com>
2023-01-19 02:00:22 +0100

    change card width for admin dashboard

--- src/Admin/Components/UI/Card.jsx
+++ src/Admin/Components/UI/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ children }) => {
   return (
-    <div className="bg-gray-200/70 rounded-lg w-[90%] shadow-card h-fit  overflow-auto mx-auto max-w-[1000px] mt-6">
+    <div className="bg-gray-200/70 rounded-lg w-[98%] shadow-card h-fit overflow-auto mx-auto max-w-[1000px] mt-6">
       {children}
     </div>
   );
